3974e61e2b28507a9edd8e1b1d770ef0e12e0fd4
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-01-27 13:59:03 +0500

    initialize quiz brain

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizBrain quizBrain = QuizBrain();
   @override
   Widget build(BuildContext context) {
     return Column(
